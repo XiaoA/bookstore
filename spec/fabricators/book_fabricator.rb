354fddd1709663f202a30bcb522a214d82bb9f65
XiaoA <abuckingham99@gmail.com>
2015-10-20 09:58:04 -0700

    Implement Books Controller

---
+++ spec/fabricators/book_fabricator.rb
@@ -0,0 +1,11 @@
+Fabricator(:book) do
+  title { Faker::Book.title }
+  isbn { Faker::Code.isbn }
+  description { Faker::Lorem.sentences(1) }
+  published_at { Faker::Date.backward }
+  publisher_id { Faker::Number.number(2) }
+  page_count { Faker::Number.number(3) }
+  price { Faker::Commerce.price }
+  created_at { Faker::Date.backward }
+  updated_at { Faker::Date.backward }
+end
