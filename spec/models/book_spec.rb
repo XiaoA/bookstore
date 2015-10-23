a5ca1afc39a78f1e76ad8e802a20aac972db8b70
XiaoA <abuckingham99@gmail.com>
2015-10-23 16:43:57 -0700

    Update 'shoulda-matchers' gem to 3.0.1

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,6 +10,5 @@ RSpec.describe Book, :type => :model do
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:published_at) }
   it { should validate_presence_of(:publisher_id) }
-  it { should validate_numericality_of(:page_count).only_integer }
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0.0) }
 end
