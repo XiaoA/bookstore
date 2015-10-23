325cc356b06f8f152e34d4fae31d3269dcdc6e68
XiaoA <abuckingham99@gmail.com>
2015-10-23 15:27:20 -0700

    Create capybara test for Book #create

---
+++ spec/features/creating_book_feature_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Books" do
+  let!(:peachpit) {Fabricate(:publisher, name: 'Peachpit Press') }
+  let!(:author1) {Fabricate(:author) }
+  let!(:author2) {Fabricate(:author) }
+
+  scenario "with valid inputs succeeds" do
+    visit root_path
+    click_link "Books", exact: true
+    click_link "Add New Book"
+
+    fill_in "Title", with: "Ruby for Awesome Web Development"
+    fill_in "Isbn", with: "9872438732921"
+    fill_in "Page count", with: 329
+    fill_in "Price", with: 39.99
+    fill_in "Description", with: "Learn how to use Ruby to build your next web project"
+    fill_in "Published at", with: "2013-09-19"
+    select "Peachpit Press", from: "Publisher"
+    check author1.full_name
+    check author2.full_name
+
+    click_button "Create Book"
+
+    expect(page).to have_content('Book has been created')
+    
+  end
+end
