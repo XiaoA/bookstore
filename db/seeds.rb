508b7d237763e76d4bcb63c9fa2e9975d0dccbbe
XiaoA <abuckingham99@gmail.com>
2015-10-26 21:42:27 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Author.create!(first_name: 'Agatha', last_name: 'Christie')
+Author.create!(first_name: 'Ernest', last_name: 'Hemingway')
+Author.create!(first_name: 'Herman', last_name: 'Melville')
+Author.create!(first_name: 'Ki no', last_name: 'Tsurayuki')
+Author.create!(first_name: 'William', last_name: 'Shakespeare')
+Author.create!(first_name: 'Maya', last_name: 'Angelou')
+
+Publisher.create!([
+                    {name: 'Wiley'},
+                    {name: 'HarperCollins'},
+                    {name: 'Cambridge University Press'},
+                    {name: 'City Lights Publishers'},
+                    {name: 'Faber and Faber'},
+                    {name: 'Macmillan Publishers'}
+                  ])
+
+Book.create!(title: "GNU Emacs for Developers", isbn: '967-1-922-88980-2', page_count: 360, price: 39.99, description: 'A book about the Emacs text editor', published_at: '2012-11-12', publisher_id: 1, book_cover: File.open(File.join(Rails.root, 'app/assets/images/emacs.jpg')))
