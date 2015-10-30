5e9b30698e8aa06efcd7e731260acaf62fafc888
XiaoA <abuckingham99@gmail.com>
2015-10-29 22:58:17 -0700

    Validate user email format with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}, format: { with: /\A[a-z0-9#_-~!$&'()*+,;=:.%]+@\.[a-z]{2,4}\z/i }
+
 
   def full_name
     "#{first_name} #{last_name}"
