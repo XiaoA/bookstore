03329fa041099455001529fe4679a1646d85d06f
XiaoA <abuckingham99@gmail.com>
2015-11-01 15:38:52 -0800

    Push authentication logic to the model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
       flash[:success] = "Sign in successful"
       redirect_to root_path
     else
-      flash[:danger] = "There was a problem with your email or password."
+      flash.now[:danger] = "There was a problem with your email or password."
       render :new
     end
   end
