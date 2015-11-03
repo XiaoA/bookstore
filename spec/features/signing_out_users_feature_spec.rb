3c4510b5d9222b225ed5b365102d689b6bc6cd94
XiaoA <abuckingham99@gmail.com>
2015-11-02 22:56:28 -0800

    Add signout feature spec

--- spec/features/signing_out_users_feature_spec.rb
+++ spec/features/signing_out_users_feature_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature 'Signing Out Users' do
-  let!(:user) { Fabricate(:user, email: 'john@example.com') }
+  let(:user) { Fabricate(:user, email: 'john@example.com') }
 
-  scenario 'successful signout' do
+  before { sign_in_as user }
+  
+  scenario do
     visit root_path
 
     click_link 'Sign out'
     expect(page).to have_content('You have successfully signed out.')
+    expect(current_path).to eq(root_path)
+    expect(page).not_to have_content("Sign out")
   end
 end
