4e951004c449a74f3603411e1af7c7c8fa04179e
XiaoA <abuckingham99@gmail.com>
2015-11-02 23:32:56 -0800

    Implement friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,11 @@ class SessionsController < ApplicationController
     if user = User.authenticate(params[:email], params[:password])
       session[:user_id] = user.id
       flash[:success] = "Sign in successful"
+      if session[:intended_destination]
+        redirect_to session[:intended_destination]
+        session[:intended_destination] = nil
+      else
+      end
       redirect_to root_path
     else
       flash.now[:danger] = "There was a problem with your email or password."
