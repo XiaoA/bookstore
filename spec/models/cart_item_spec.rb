1ac73a3352873b9df25338303e853cf08eaf607d
XiaoA <abuckingham99@gmail.com>
2015-11-10 23:32:01 -0800

    Implement 'line total' and 'total sale' methods

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe CartItem, type: :model do
   it { should belong_to(:cart) }
   it { should belong_to(:book) }
+
+  describe "#line_total" do
+    it "should return the line total cost" do
+      book = Fabricate(:book, price: 30)
+      cart = Fabricate(:cart)
+      cart_item = Fabricate(:cart_item, cart_id: cart.id, book_id: book.id, price: book.price, quantity: 10)
+
+      expect(cart_item
+    end
+  end
+
 end
