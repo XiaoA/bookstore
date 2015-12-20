25ce2e93dda476ad00cacea7f0883b9f942234b8
XiaoA <abuckingham99@gmail.com>
2015-12-19 17:26:43 -0800

    Implement order confirmation mail feature

--- spec/mailers/previews/order_mailer_preview.rb
+++ spec/mailers/previews/order_mailer_preview.rb
@@ -1,8 +1,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/order_mailer
 class OrderMailerPreview < ActionMailer::Preview
   def notify_on_successful_order
-    customer = Customer.Last
-    order = Order.Last
+    customer = User.create(first_name: 'Sherlock', last_name: 'Holmes')
+    publisher = Publisher.create(name: "Pendant Publishing")
+    book1 = Book.create(title: 'First book',price: 10, isbn: '1111111111111', page_count: 100, description: "My first book", publisher:publisher)
+    book2 = Book.create(title: 'Second book',price: 15, isbn: '1111111111112', page_count: 150, description: "My second book", publisher:publisher)
+    order = Order.create
+    order_item1 = order.order_items.create(book: book1, quantity: 2, price: book1.price)
+    order_item2 = order.order_items.create(book: book2, quantity: 1, price: book2.price)
+
     OrderMailer.notify_on_successful_order(customer, order)
   end
 end
